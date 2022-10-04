e9df2f4d7fb6c5e72f3bd95ef24512833077ee1a
Rafael Rodrigues Bento <rafael.rodrigues.bento@outlook.com>
2022-10-04 00:33:51 -0300

    Implantado autenticação JWT

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -2,14 +2,18 @@ const { Router } = require('express');
 
 DishesController = require('../controllers/DishesController');
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const ensureAdmin = require('../middlewares/ensureAdmin');
+
 const dishRoutes = Router();
 
 const dishesController = new DishesController();
 
-dishRoutes.post('/', dishesController.create);
+dishRoutes.use(ensureAuthenticated);
+dishRoutes.post('/', ensureAdmin, dishesController.create);
 dishRoutes.get('/:id', dishesController.show);
 dishRoutes.get('/', dishesController.index);
 dishRoutes.put('/:id', dishesController.update);
-dishRoutes.delete('/:id', dishesController.delete);
+dishRoutes.delete('/:id', ensureAdmin, dishesController.delete);
 
 module.exports = dishRoutes;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 
 const userRoutes = require('./users.routes');
 const dishesRoutes = require('./dishes.routes');
+const sessionsRoutes = require('./sessions.routes');
 
 const routes = Router();
 
+routes.use('/sessions', sessionsRoutes);
 routes.use('/users', userRoutes);
 routes.use('/dishes', dishesRoutes);
 
