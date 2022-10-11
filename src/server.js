f76265d829be01a115629baf51f9ac8f1255cf03
Rafael Rodrigues Bento <rafael.rodrigues.bento@outlook.com>
2022-10-10 23:43:58 -0300

    Preparado para deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 require('express-async-errors');
+
+require('dotenv/config');
+
 const express = require('express');
 const cors = require('cors');
 const database = require('./database/sqlite');
@@ -34,6 +37,6 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`server is running at ${PORT}`));
